53a4f0168d825a6a2c8bea2da103c8b855142049
Zoocharie <fleshogre@gmail.com>
2022-07-11 21:33:12 +0800

    Project Setup [11 July 2022]
    - Added GLEW, GLM, GLFW
    - Added ImGui with a demo
    - Setup GITIGNORE

---
+++ Engine/core-engine/src/main.cpp
@@ -0,0 +1,229 @@
+/*!
+@file    main.cpp
+@author  pghali@digipen.edu
+@date    10/11/2016
+
+This file uses functionality defined in types GLHelper and GLApp to initialize 
+an OpenGL context and implement a game loop.
+
+*//*__________________________________________________________________________*/
+
+/*                                                                   includes
+----------------------------------------------------------------------------- */
+// Extension loader library's header must be included before GLFW's header!!!
+#include <glhelper.h>
+#include <glapp.h>
+#include <iostream>
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+/*                                                   type declarations
+----------------------------------------------------------------------------- */
+
+/*                                                      function declarations
+----------------------------------------------------------------------------- */
+static void draw();
+static void update();
+static void init();
+static void cleanup();
+
+
+ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+/*                                                      function definitions
+----------------------------------------------------------------------------- */
+/*  _________________________________________________________________________ */
+/*! main
+
+@param none
+
+@return int
+
+Indicates how the program existed. Normal exit is signaled by a return value of
+0. Abnormal termination is signaled by a non-zero return value.
+Note that the C++ compiler will insert a return 0 statement if one is missing.
+*/
+int main() {
+
+  // Part 1
+  init();
+
+  //imgui
+
+
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO(); (void)io;
+  ImGui::StyleColorsDark();
+  ImGui_ImplGlfw_InitForOpenGL(GLHelper::ptr_window, true);
+  ImGui_ImplOpenGL3_Init("#version 330");
+
+  // Our state
+  bool show_demo_window = true;
+  bool show_another_window = false;
+  // Part 2
+  while (!glfwWindowShouldClose(GLHelper::ptr_window)) {
+
+    // Rendering
+
+    update();
+
+    // Start the Dear ImGui frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+    if (show_demo_window)
+        ImGui::ShowDemoWindow(&show_demo_window);
+
+    // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+    {
+        static float f = 0.0f;
+        static int counter = 0;
+
+        ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+        ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+        ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+        ImGui::Checkbox("Another Window", &show_another_window);
+
+        ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+        ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+        if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+            counter++;
+        ImGui::SameLine();
+        ImGui::Text("counter = %d", counter);
+
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        ImGui::End();
+    }
+
+    // 3. Show another simple window.
+    if (show_another_window)
+    {
+        ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+        ImGui::Text("Hello from another window!");
+        if (ImGui::Button("Close Me"))
+            show_another_window = false;
+        ImGui::End();
+    }
+
+    // Part 2b
+    draw();
+  }
+
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
+
+  // Part 3
+  cleanup();
+}
+
+// Dear ImGui: standalone example application for GLFW + OpenGL 3, using programmable pipeline
+// (GLFW is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
+// If you are new to Dear ImGui, read documentation from the docs/ folder + read the top of imgui.cpp.
+// Read online: https://github.com/ocornut/imgui/tree/master/docs
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <stdio.h>
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+#endif
+#include <GLFW/glfw3.h> // Will drag system OpenGL headers
+
+// [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
+// To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
+// Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, "legacy_stdio_definitions")
+#endif
+
+static void glfw_error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+}
+
+
+/*  _________________________________________________________________________ */
+/*! update
+@param none
+@return none
+
+Uses GLHelper::GLFWWindow* to get handle to OpenGL context.
+For now, there are no objects to animate nor keyboard, mouse button click,
+mouse movement, and mouse scroller events to be processed.
+*/
+static void update() {
+  // Part 1
+  glfwPollEvents();
+
+  // Part 2
+  GLHelper::update_time(1.0);
+  
+  // Part 3
+  GLApp::update();
+}
+
+/*  _________________________________________________________________________ */
+/*! draw
+@param none
+@return none
+
+Call application to draw and then swap front and back frame buffers ...
+Uses GLHelper::GLFWWindow* to get handle to OpenGL context.
+*/
+static void draw() {
+  // Part 1
+  GLApp::draw();
+
+  ImGui::Render();
+  ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+  // Part 2: swap buffers: front <-> back
+  glfwSwapBuffers(GLHelper::ptr_window);
+}
+
+/*  _________________________________________________________________________ */
+/*! init
+@param none
+@return none
+
+The OpenGL context initialization stuff is abstracted away in GLHelper::init.
+The specific initialization of OpenGL state and geometry data is
+abstracted away in GLApp::init
+*/
+static void init() {
+  // Part 1
+  if (!GLHelper::init(2400, 1350, "Tutorial 1")) {
+    std::cout << "Unable to create OpenGL context" << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+
+  // Part 2
+  GLHelper::print_specs();
+
+  // Part 3
+  GLApp::init();
+}
+
+/*  _________________________________________________________________________ */
+/*! cleanup
+@param none
+@return none
+
+Return allocated resources for window and OpenGL context thro GLFW back
+to system.
+Return graphics memory claimed through 
+*/
+void cleanup() {
+  // Part 1
+  GLApp::cleanup();
+
+  // Part 2
+  GLHelper::cleanup();
+}
