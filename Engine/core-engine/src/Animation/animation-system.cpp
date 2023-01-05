fcb4b07dac9add3ccf7c167da74ba8a69f2d1872
Sean.Ngo <Prawnny@live.com>
2023-01-05 15:47:22 +0800

    [Feat] Setup animation system
    - Created files for animation system

---
+++ Engine/core-engine/src/Animation/animation-system.cpp
@@ -0,0 +1,24 @@
+/*!***************************************************************************************
+\file			animation-system.cpp
+\project
+\author			Sean Ngo
+
+\par			Course: GAM200
+\par			Section:
+\date			05/01/2023
+
+\brief
+	This file holds the definition of the Animation system. The Animation system animates
+	the spritesheets in the engine, where the user has to create the animation in the
+	animator in the editor.
+
+All content © 2023 DigiPen Institute of Technology Singapore. All rights reserved.
+*****************************************************************************************/
+#include "pch.h"
+
+#include "Animation/animation-system.h"
+
+namespace Copium
+{
+
+}
---
+++ Engine/core-engine/src/Animation/animation-system.h
@@ -0,0 +1,25 @@
+/*!***************************************************************************************
+\file			animation-system.h
+\project
+\author			Sean Ngo
+
+\par			Course: GAM200
+\par			Section:
+\date			05/01/2023
+
+\brief
+	This file holds the declaration of the Animation system. The Animation system animates
+	the spritesheets in the engine, where the user has to create the animation in the 
+	animator in the editor.
+
+All content © 2023 DigiPen Institute of Technology Singapore. All rights reserved.
+*****************************************************************************************/
+#ifndef ANIMATION_SYSTEM_H
+#define ANIMATION_SYSTEM_H
+
+namespace Copium
+{
+
+}
+
+#endif // !ANIMATION_SYSTEM_H
