5da56be2336115258521026496a0cfc790a1ef50
BlessedOfNyx <92707787+BlessedOfNyx@users.noreply.github.com>
2023-01-26 15:54:54 +0800

    [Feat] New unit test
    Crashes when I try to add the C# Script "Unit" as a component

--- Engine/PackedTracks/Assets/Scripts/Unit.cs
+++ Engine/PackedTracks/Assets/Scripts/Unit.cs
@@ -1,141 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using CopiumEngine;
-using static GameData;
+using CopiumEngine.UI;
+using System;
 
-public class Unit
+public class Unit : CopiumScript
 {
-    public string Name { get; set; }
+    public string unitName;
+    static int count = 0;
 
-    public int MaxHp { get; set; }
-    public int Hp { get; set; }
+    public int healthState = 2;
+    // Health state ranges from 0 to 2
+    // 2 - Healthy, able to take some hits
+    // 1 - Injured, took some hits
+    // 0 - Critical, can possibly die
 
-    public int TempHp { get; set; } = 0;
+    public int mentalState = 2;
+    // Mental state ranges from 0 to 2
+    // 2 - Sound, haven't encountered anything too bad
+    // 1 - Shaken, might trigger some events, increase chance to miss shots
+    // 0 - Broken, might trigger some events, might ignore commands totally
 
-    public Skill Skill { get; set; }
-    public Bullet Bullet { get; set; }
-    public Gun Gun { get; set; }
+    public int hungerState = 1;
+    // Hunger state ranges from 0 to 1
+    // 1 - Fed, will increase health state next day, chance to increase mental state
+    // 0 - Famished, will decrease health state next day, might trigger hunger events
 
-    public bool IsDead { get; set; } = false;
-
-    public Unit(string name, int maxHp, Bullet bullet, Gun gun, Skill skill)
+    public void Start()
     {
-        Name = name;
-        MaxHp = maxHp;
-        Hp = MaxHp;
-
-        Bullet = bullet;
-        Gun = gun;
-        Skill = skill;
-    }
-}
-
-public static class Unitf
-{
-    public static void DamageHeat(int dmg)
-    {
-        Heat -= dmg;
-
-        if (Heat <= 0)
+        if (count == 0)
         {
-            Heat = 0;
+            unitName = "Bob";
         }
-    }
-    public static void DamageHeat(Bullet bullet)
-    {
-        Heat -= bullet.Dmg;
-
-        if (Heat <= 0)
-        {
-            Heat = 0;
-        }
-    }
-    public static void PrepFreshUnit(Unit target)
-    {
-        target.Hp = target.MaxHp;
-    }
-
-    public static void DamageUnit(Unit target, int dmg)
-    {
-        target.Hp -= dmg;
-
-        if (target.Hp <= 0)
-        {
-            target.Hp = 0;
-            target.IsDead = true;
-        }
-
-    }
-    public static void DamageUnit(Unit target, Bullet bullet)
-    {
-        target.Hp -= bullet.Dmg;
-
-        if (target.Hp <= 0)
-        {
-            target.Hp = 0;
-            target.IsDead = true;
-        }
-
-    }
-
-    public static void HealUnit(Unit target, int heal)
-    {
-        target.Hp += heal;
-
-        if (target.IsDead == true)
-        {
-            target.IsDead = false;
-        }
-
-        if (target.Hp > target.MaxHp)
-        {
-            target.Hp = target.MaxHp;
-        }
-    }
-    
-    public static void CombatTempHpMod(Unit target, int mod)
-    {
-        target.TempHp += mod;
-
-        target.Hp += mod;
-        target.MaxHp += mod;
-    }
-    public static void EndCombatTempHpMod(Unit target)
-    {
-        target.Hp -= target.TempHp;
-
-        if (target.Hp <= 0)
-        {
-            target.Hp = 1;
-        }
-
-        target.MaxHp -= target.TempHp;
-
-        if (target.MaxHp <= 0)
-        {
-            target.MaxHp = 1;
-        }
-
-        target.TempHp = 0;
-    }
-    
-    public static void PermHpMod(Unit unit, int mod)
-    {
-        unit.Hp += mod;
-        unit.MaxHp += mod;
-    }
-
-    public static void ChangeSkill(Unit unit, Skill mod)
-    {
-        unit.Skill = mod;
-    }
-    public static void ChangeGun(Unit unit, Gun mod)
-    {
-        unit.Gun = mod;
-    }
-    public static void ChangeBullet(Unit unit, Bullet mod)
-    {
-        unit.Bullet = mod;
     }
 }
