916a99b69272113b153a886de1257ab35f1aee73
Fappyzz <92708533+Fappyzz@users.noreply.github.com>
2023-03-07 00:55:03 +0800

    Changed Main Menu
    Added SFX for buttons and changed overall look of main menu

--- Engine/PackedTracks/Assets/Scripts/MainMenu.cs
+++ Engine/PackedTracks/Assets/Scripts/MainMenu.cs
@@ -3,14 +3,26 @@ using System;
 
 public class MainMenu: CopiumScript
 {
-    public AudioSource audio;
+    public AudioSource bgm_MainMenu;
+    public AudioSource sfx_ButtonHover;
+    public AudioSource sfx_ButtonClick;
     public Button StartGameButton;
     public Button QuitGameButton;
     public Button HowtoPlayButton;
 
+    public GameObject arrowLocStart;
+    public GameObject arrowLocHTP;
+    public GameObject arrowLocQuit;
+
+    public GameObject arrow;
+
+    private bool hovering = false;
+    private bool canPlayHoverSound = true;
+
     void Start()
     {
-        audio.Play();
+        bgm_MainMenu.Play();
+        arrow.SetActive(false);
     }
 
     void Update()
@@ -28,6 +40,39 @@ public class MainMenu: CopiumScript
         if (QuitGameButton.state == ButtonState.OnClick)
         {
             Application.Quit();
+        }
+
+        if (StartGameButton.state == ButtonState.OnHover || HowtoPlayButton.state == ButtonState.OnHover || QuitGameButton.state == ButtonState.OnHover)
+        {
+            hovering = true;
+        }
+        else
+        {
+            arrow.SetActive(false);
+            hovering = false;
+            canPlayHoverSound = true;
+        }
+
+        if (hovering && canPlayHoverSound)
+        {
+            sfx_ButtonHover.Play();
+            canPlayHoverSound = false;
+        }
+
+        if (StartGameButton.state == ButtonState.OnHover)
+        {
+            arrow.SetActive(true);
+            arrow.transform.position = arrowLocStart.transform.position;
+        }
+        if(HowtoPlayButton.state == ButtonState.OnHover)
+        {
+            arrow.SetActive(true);
+            arrow.transform.position = arrowLocHTP.transform.position;
+        }
+        if(QuitGameButton.state == ButtonState.OnHover)
+        {
+            arrow.SetActive(true);
+            arrow.transform.position = arrowLocQuit.transform.position;
         }
     }
 }
