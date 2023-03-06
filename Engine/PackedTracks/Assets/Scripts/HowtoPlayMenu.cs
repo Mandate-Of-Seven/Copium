9380fbd8faeead92fe2dff8994d31cd4712f7392
Fappyzz <92708533+Fappyzz@users.noreply.github.com>
2023-03-07 04:20:16 +0800

    Changed tut layout
    changed tutorial how it looks

--- Engine/PackedTracks/Assets/Scripts/HowtoPlayMenu.cs
+++ Engine/PackedTracks/Assets/Scripts/HowtoPlayMenu.cs
@@ -9,6 +9,12 @@ public class HowtoPlayMenu: CopiumScript
     public AudioSource sfx_pageflip;
     public GameObject prevButtonObject;
 
+    public GameObject MainPage;
+    public GameObject Page2;
+    public GameObject NextPage;
+    public GameObject PrevPage;
+
+
     int page = 1;
 
     void Start()
@@ -37,13 +43,33 @@ public class HowtoPlayMenu: CopiumScript
         
 
         }
+
+        if (NextButton.state == ButtonState.OnHover)
+        {
+            NextPage.SetActive(true);
+        }
+        else
+        {
+            NextPage.SetActive(false);
+        }
+
+        if (PrevButton.state == ButtonState.OnHover)
+        {
+            PrevPage.SetActive(true);
+        }
+        else
+        {
+            PrevPage.SetActive(false);
+        }
     }
 
     void ShowPage(int _page)
     {
         if (page == 1)
         {
-             text.text = "Use the mouse to interact with objects in the train cockpit\n " +
+            MainPage.SetActive(true);
+            Page2.SetActive(false);
+            text.text = "Use the mouse to interact with objects in the train cockpit\n " +
                      "An alert will be shown if a new event has occured.\n " +
                      "In the cockpit, there is a meter to show how far you are\n " +
                      "from the destination.";
@@ -52,6 +78,8 @@ public class HowtoPlayMenu: CopiumScript
 
         if (page == 2)
         {
+            Page2.SetActive(true);
+            MainPage.SetActive(false);
             text.text = "Every few kilometers, there will be an event that happens.\n " +
                              "You can go to the event menu from the middle screen in the \n" +
                              "cockpit to make a decision for the event. Your choices can\n" +
@@ -59,16 +87,16 @@ public class HowtoPlayMenu: CopiumScript
                 prevButtonObject.SetActive(true);
         }
 
+        //if (page == 3)
+        //{
+        //     text.text = "The left panel will show the status of your crew. There are\n"+
+        //                     "three attributes of your crew members to take note of, and\n"  +
+        //                     "their condition can determine whether they live or die.\n\n" +
+        //                     "In addition, you can send your crew out to explore for supplies.\n" +
+        //                     "When exploring, a random event can occur to them, which will\n" +
+        //                     "impact their status.";
+        //}
         if (page == 3)
-        {
-             text.text = "The left panel will show the status of your crew. There are\n"+
-                             "three attributes of your crew members to take note of, and\n"  +
-                             "their condition can determine whether they live or die.\n\n" +
-                             "In addition, you can send your crew out to explore for supplies.\n" +
-                             "When exploring, a random event can occur to them, which will\n" +
-                             "impact their status.";
-        }
-        if (page == 4)
         {
               SceneManager.LoadScene("MainMenu");
         }
