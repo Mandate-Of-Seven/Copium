2f7016094e4e44af5cd70a2e261a843964321cd0
icepie100 <icepiepotato@gmail.com>
2023-03-07 02:03:46 +0800

    [Feat] added audio to how to play scene
    -audio will play when button press on how to play

--- Engine/PackedTracks/Assets/Scripts/HowtoPlayMenu.cs
+++ Engine/PackedTracks/Assets/Scripts/HowtoPlayMenu.cs
@@ -6,6 +6,7 @@ public class HowtoPlayMenu: CopiumScript
     public Text text;
     public Button NextButton;
     public Button PrevButton;
+    public AudioSource sfx_pageflip;
     public GameObject prevButtonObject;
 
     int page = 1;
@@ -22,36 +23,56 @@ public class HowtoPlayMenu: CopiumScript
 
     void Update()
     {
+        if (PrevButton.state == ButtonState.OnClick)
+        {   
+            sfx_pageflip.Play();
+            page--;
+            ShowPage(page);
+        }
         if (NextButton.state == ButtonState.OnClick)
         {
-            switch (page)
-            {
-                case 1:
-                text.text = "Every few kilometers, there will be an event that happens.\n " +
+            sfx_pageflip.Play();
+            page++;
+            ShowPage(page);
+        
+
+        }
+    }
+
+    void ShowPage(int _page)
+    {
+        if (page == 1)
+        {
+             text.text = "Use the mouse to interact with objects in the train cockpit\n " +
+                     "An alert will be shown if a new event has occured.\n " +
+                     "In the cockpit, there is a meter to show how far you are\n " +
+                     "from the destination.";
+                     prevButtonObject.SetActive(false);
+        }
+
+        if (page == 2)
+        {
+            text.text = "Every few kilometers, there will be an event that happens.\n " +
                              "You can go to the event menu from the middle screen in the \n" +
                              "cockpit to make a decision for the event. Your choices can\n" +
                              "have an impact on your journey to your destination.\n";
                 prevButtonObject.SetActive(true);
-                page = 2;
-                break;
+        }
 
-                case 2:
-                text.text = "The left panel will show the status of your crew. There are\n"+
+        if (page == 3)
+        {
+             text.text = "The left panel will show the status of your crew. There are\n"+
                              "three attributes of your crew members to take note of, and\n"  +
                              "their condition can determine whether they live or die.\n\n" +
                              "In addition, you can send your crew out to explore for supplies.\n" +
                              "When exploring, a random event can occur to them, which will\n" +
                              "impact their status.";
-                page = 3;
-                break;
-
-                case 3:
-                SceneManager.LoadScene("MainMenu");
-                break;
-
-            }
-
         }
+        if (page == 4)
+        {
+              SceneManager.LoadScene("MainMenu");
+        }
+
     }
 
 }
