ff388926b8e0d4dcf439c5789ce1b4403b39f3cb
icepie100 <icepiepotato@gmail.com>
2023-03-07 01:47:02 +0800

    [Feat] Added text to how to play
    -Added some text describing parts of the game in how to play.

--- Engine/PackedTracks/Assets/Scripts/HowtoPlayMenu.cs
+++ Engine/PackedTracks/Assets/Scripts/HowtoPlayMenu.cs
@@ -1,6 +1,6 @@
 using CopiumEngine;
 using System;
-
+using System.Runtime.Remoting.Messaging;
 public class HowtoPlayMenu: CopiumScript
 {
     public Text text;
@@ -13,7 +13,10 @@ public class HowtoPlayMenu: CopiumScript
     void Start()
 	{
 		prevButtonObject.SetActive(false);
-        text.text = "train cockpit info goes here";
+        text.text = "Use the mouse to interact with objects in the train cockpit\n " +
+                     "An alert will be shown if a new event has occured.\n " +
+                     "In the cockpit, there is a meter to show how far you are\n " +
+                     "from the destination.";
 	}
 
 
@@ -24,13 +27,21 @@ public class HowtoPlayMenu: CopiumScript
             switch (page)
             {
                 case 1:
-                text.text = "Events go here";
+                text.text = "Every few kilometers, there will be an event that happens.\n " +
+                             "You can go to the event menu from the middle screen in the \n" +
+                             "cockpit to make a decision for the event. Your choices can\n" +
+                             "have an impact on your journey to your destination.\n";
                 prevButtonObject.SetActive(true);
                 page = 2;
                 break;
 
                 case 2:
-                text.text = "Stuff about the crew panel goes here";
+                text.text = "The left panel will show the status of your crew. There are\n"+
+                             "three attributes of your crew members to take note of, and\n"  +
+                             "their condition can determine whether they live or die.\n\n" +
+                             "In addition, you can send your crew out to explore for supplies.\n" +
+                             "When exploring, a random event can occur to them, which will\n" +
+                             "impact their status.";
                 page = 3;
                 break;
 
